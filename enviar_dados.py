842468a3d5f9660c3e0dcba0ca235e0d18c3cc1c
Bia Astle <astlebia@gmail.com>
2025-10-29 15:09:16 -0300

    script atualizado

--- enviar_dados.py
+++ enviar_dados.py
@@ -1,12 +1,11 @@
 import basedosdados as bd
-import json
+import numpy as np
 import requests
 
 query = """
-SELECT pm.id_municipio, pm.ano, pm.pib AS pib_total,ROUND(pm.pib/pop.populacao,2) AS pib_per_capita ,pm.va_agropecuaria,pm.va_industria,pm.va_servicos, ROUND(pm.va_agropecuaria/pm.va * 100,2) AS participacao_agropecuaria, ROUND(pm.va_industria/pm.va * 100,2) AS participacao_industria, ROUND(pm.va_servicos/pm.va * 100,2) AS participacao_servicos 
-FROM `basedosdados.br_ibge_pib.municipio` AS pm
-INNER JOIN `basedosdados.br_ibge_populacao.municipio` AS pop ON pm.id_municipio = pop.id_municipio AND pm.ano = pop.ano
-WHERE pm.ano>=2010;
+SELECT id_municipio, ano, produto AS nome_produto, area_destinada_colheita, area_colhida, quantidade_produzida, rendimento_medio_producao, valor_producao
+FROM `basedosdados.br_ibge_pam.lavoura_permanente` 
+WHERE ano>= 2010 AND (area_destinada_colheita IS NOT NULL OR area_colhida IS NOT NULL OR quantidade_produzida IS NOT NULL OR rendimento_medio_producao IS NOT NULL OR valor_producao IS NOT NULL);
 """
 
 
@@ -17,11 +16,11 @@ df = bd.read_sql (
 )
 
 
-df = df.fillna(0)
+df = df.replace({np.nan: None})
 data = df.to_dict(orient='records')
 
 batch_size = 5000
-url = 'http://172.19.4.145:9000/panorama-economico/pib-setores'
+url = 'http://172.19.4.145:9000/panorama-economico/producao-agricola-permanente'
 
 print(f"Total: {len(data)} registros")
 

f27f19e923f6c06517e3c2062d86faa2885503b6
Bia Astle <astlebia@gmail.com>
2025-11-21 14:05:52 -0300

    script atualizado

--- enviar_dados.py
+++ enviar_dados.py
@@ -3,28 +3,9 @@ import numpy as np
 import requests
 
 query = """
-SELECT 
-    id_municipio, 
-    ano, 
-    CASE WHEN rede = '1' THEN 'FEDERAL' WHEN rede = '2' THEN 'ESTADUAL' WHEN rede = '3' THEN 'MUNICIPAL' END AS rede, 
-    COALESCE(SUM(quantidade_matricula_infantil), 0) AS estudantes_infantil, 
-    COALESCE(SUM(quantidade_matricula_fundamental), 0) AS estudantes_fundamental, 
-    COALESCE(SUM(quantidade_matricula_medio), 0) AS estudantes_medio,
-    COALESCE(SUM(quantidade_matricula_profissional), 0) AS estudantes_profissional,
-    COALESCE(SUM(quantidade_matricula_eja), 0) AS estudantes_eja, 
-    COALESCE(SUM(quantidade_docente_infantil), 0) AS docentes_infantil, 
-    COALESCE(SUM(quantidade_docente_fundamental), 0) AS docentes_fundamental, 
-    COALESCE(SUM(quantidade_docente_medio), 0) AS docentes_medio,
-    COALESCE(SUM(quantidade_docente_profissional), 0) AS docentes_profissional,
-    COALESCE(SUM(quantidade_docente_eja), 0) AS docentes_eja, 
-    COUNTIF(etapa_ensino_infantil = 1) AS escolas_infantil, 
-    COUNTIF(etapa_ensino_fundamental = 1) AS escolas_fundamental, 
-    COUNTIF(etapa_ensino_medio = 1) AS escolas_medio, 
-    COUNTIF(etapa_ensino_profissional = 1) AS escolas_profissional, 
-    COUNTIF(etapa_ensino_eja = 1) AS escolas_eja
-FROM `basedosdados.br_inep_censo_escolar.escola` 
-WHERE ano >= 2010 AND rede != '4'
-GROUP BY ano, id_municipio, rede;
+SELECT id_municipio, ano, id_escola, rede, ensino, ideb
+FROM `basedosdados.br_inep_ideb.escola`
+WHERE ano >= 2010 AND ideb IS NOT NULL;
 """
 
 
@@ -38,7 +19,7 @@ df = df.replace({np.nan: None})
 data = df.to_dict(orient='records')
 
 batch_size = 10000
-url = 'http://172.19.4.145:9000/dados-educacionais/oferta-educacional'
+url = 'http://172.19.4.145:9000/dados-educacionais/ideb-escola'
 
 print(f"Total: {len(data)} registros")
 

5c475a6b5a563303852204b80246894cbb3ce005
Bia Astle <astlebia@gmail.com>
2025-11-21 10:41:55 -0300

    feat: endpoints para ideb

--- app/routers/dados_educacionais.py
+++ app/routers/dados_educacionais.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from sqlmodel import Session, select
 from ..database import SessionDep
-from ..models import Escolas, OfertaEducacional
-from ..schemas import EscolasCreate, OfertaEducacionalCreate
+from ..models import Escolas, IdebEscola, OfertaEducacional
+from ..schemas import EscolasCreate, IdebEscolaCreate, OfertaEducacionalCreate
 
 router = APIRouter(prefix="/dados-educacionais", tags=["Dados Educacionais"])
 
@@ -39,6 +39,25 @@ def create_oferta_educacional(dados: list[OfertaEducacionalCreate] | OfertaEduca
         return {"inserted": len(novos_dados)}
     else:
         novo_dado = OfertaEducacional(**dados.model_dump())
+        session.add(novo_dado)
+        session.commit()
+        session.refresh(novo_dado)
+        return novo_dado
+
+@router.get("/ideb-escola")
+def read_ideb_escola(session: SessionDep) -> list[IdebEscola]:
+    dados = session.exec(select(IdebEscola)).all()
+    return dados
+
+@router.post("/ideb-escola")
+def create_ideb_escola(dados: list[IdebEscolaCreate] | IdebEscolaCreate, session: SessionDep) -> dict:
+    if isinstance(dados, list):
+        novos_dados = [IdebEscola(**dado.model_dump()) for dado in dados]
+        session.add_all(novos_dados)
+        session.commit()
+        return {"inserted": len(novos_dados)}
+    else:
+        novo_dado = IdebEscola(**dados.model_dump())
         session.add(novo_dado)
         session.commit()
         session.refresh(novo_dado)
